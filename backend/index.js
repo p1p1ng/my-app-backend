9b435c57c24f1c527942394b7abf46cb9013f9b6
p1p1ng <you@example.com>
2025-03-20 23:01:36 +0700

    update

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
+// import db from "./config/Database.js";
+import UserRoute from "./routes/UserRoute.js";
+import ProductRoute from "./routes/ProductRoute.js";
+
+dotenv.config(); // function dontenv to load environment variables
+
+const app = express();
+
+// (async() => {
+//     await db.sync();
+// })();
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: "auto"
+    }
+}));
+
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}));
+app.use(express.json());
+app.use(UserRoute);
+app.use(ProductRoute);
+
+// app.use("/users", UserRoute);
+// app.use("/products", ProductRoute);
+
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Server running on port ${process.env.APP_PORT}`);
+});
