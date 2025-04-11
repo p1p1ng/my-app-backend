ec055b3c2a4b4667afe68d3422905980b4967414
p1p1ng <firmanarif12345@gmail.com>
2025-04-11 21:52:37 +0700

    New update

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './Layout'
+
+const Dashboard = () => {
+  return (
+    <Layout>
+        Lorem, ipsum.
+    </Layout>
+  )
+};
+
+export default Dashboard
